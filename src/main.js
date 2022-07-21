8dea4e2f9ab6b82319ab4c0536bbecdf7a07b8ca
Paweł <pawel.witek@frontkom.com>
2022-07-21 04:31:05 +0200

    added dynamic fetch content select picker

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ class Post_Picker_Field extends Component {
         super(props);
         this.state = {
             arr: ['one','two','trhee'],
-            items: ['one','two','trhee'],
+            items: [],
             item: "lol"
         };
     }
@@ -27,24 +27,29 @@ class Post_Picker_Field extends Component {
         onChange(id, e.target.value);
     }
 
-    fetchPosts = (e) => {
-        let items = [];
+    fetchPosts = () => {
         apiFetch({ path: '/wp-json/wp/v2/posts' }).then((posts) => {
+            let items = [];
             posts.forEach(element => {
                 let item = {};
                 item.id = element.id || '';
                 item.title = element.title.rendered || '';
                 items.push(item.title);
             });
+            // ZAWSZE TUTAJ MUSI BYĆ SET STATE !!!
+            this.setState({ items: posts })
         });
-        return items;
     }
     renderOptions = (items) => {
         let itemsHtml = items.map(function (item) {
             return <option key="{id}" value={item} className="option">{item}</option>
         })
-
         return itemsHtml;
+    }
+    // To jest lifecycle w klasie
+    // w funkcji js jako component zastąpione jest useState i useEffect TODO do doczytania
+    componentDidMount() {
+        this.fetchPosts();
     }
 
     render() {
@@ -60,17 +65,16 @@ class Post_Picker_Field extends Component {
             <>
                 <div className="input-wrapper d-flex nowrap align-items-center crb_field">
                     <select name={name} id={id}>
-                        {this.renderOptions(this.state.arr)}
+                    {this.state.items.map((item) => (
+                        <option key={item.id}>{item.title.rendered}</option>
+                    ))}
                     </select>
                 </div>
             </>
         );
     }
     // lifecycle: use to update props
-    componentDidMount() {
-        let posts = this.fetchPosts();
-        this.setState({ items: posts });
-    }
+    
 }
 
 export default Post_Picker_Field;
